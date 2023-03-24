132fb3836d8c81dc6e4566c2e5119a7cfce9acd0
jasmineadm <118563254+jasmineadm@users.noreply.github.com>
2023-03-24 12:44:40 +0100

    Update Commande.java

--- Commande.java
+++ Commande.java
@@ -1,10 +1,8 @@
 package Commande;
-import java.util.Scanner;
+
 
 public class Commande {
 	
-	public String Entrée="E";
-	public String Sortir="S";
 	public String Sauvegarde="S";
 	public String Fin="F";
 
