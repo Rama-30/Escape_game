f839a164cb0505dada300860d280c89b4370b862
Rama-30 <127587741+Rama-30@users.noreply.github.com>
2023-03-24 15:48:44 +0100

    Update Zone.java

--- Zone.java
+++ Zone.java
@@ -9,21 +9,26 @@ public class Zone {
 	//private Objet[] lesObjets;
 //	private Enigme[] lesEnigmes;
 	
-	public Zone [] LesZones;
+	public String [] LesZones;
 	
 	public Zone()
 	{
-		LesZones = new Zone[this.nbrZone];
-		int i=0;
-		while(i<=this.nbrZone)
-		{
-			LesZones[i] = new Zone();
-			i++;
-		}
+		LesZones = new String[this.nbrZone];
 		
+		LesZones[0]="la Cellule";
+		LesZones[1]="le Bureau du commandant";
+		LesZones[2]="la Cantine";
+		LesZones[3]="le Salle de pause";
+		LesZones[4]="le Ponton1";
+		LesZones[5]="le Ponton2";
+		LesZones[6]="le Ponton3";
+		LesZones[7]="le Bateau";
+		LesZones[8]="le Forêt";
+		LesZones[9]="le Hangar";
+
 	}
 	
 	
-	
+	 
 	
 }
